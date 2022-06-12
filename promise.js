3284d1b64c87cce0b710102157c4a85b84cb17f3
regycloud <regycloud@yahoo.com>
2022-06-12 23:23:19 +0700

    challenge 7 Promise is done

--- promise.js
+++ promise.js
@@ -1,7 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let hasil = 0;
+  await promiseTheaterIXX(emosi).then(data => {
+    // console.log(data)
+    data.map((film) => {
+      if (film.hasil == emosi) {
+        hasil += 1;
+      }
+    })
+  });
+
+  await promiseTheaterVGC(emosi).then(data => {
+    data.map((film) => {
+      if (film.hasil == emosi) {
+        hasil += 1;
+      }
+    })
+  })
+  return hasil
+};
 
 module.exports = {
   promiseOutput,
